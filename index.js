3fb6504fe4ab5090b050c30ec87af0aeffd10cd7
Renato Vicencio N <renatovicencion@gmail.com>
2022-08-16 23:23:43 -0400

    Uncomment console.log's

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-    // console.log(`User Connected: ${socket.id}`);
+    console.log(`User Connected: ${socket.id}`);
     socket.on("join_room", (data) => {
         socket.join(data);
         console.log(`User with ID: ${socket.id} joined room: ${data}`);
@@ -27,7 +27,7 @@ io.on("connection", (socket) => {
     });
 
     socket.on("disconnect", () => {
-        // console.log("User Disconnected", socket.id);
+        console.log("User Disconnected", socket.id);
     });
 });
 
