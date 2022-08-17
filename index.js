285e0edf5ea9d14c12b648a698046ee506a34c35
Renato Vicencio N <renatovicencion@gmail.com>
2022-08-16 22:44:15 -0400

    Deploy vercel

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ io.on("connection", (socket) => {
     // console.log(`User Connected: ${socket.id}`);
     socket.on("join_room", (data) => {
         socket.join(data);
-        console.log(`User with ID: ${socket.id} joined room: ${data}`)
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
     });
 
     socket.on("send_message", (data) => {
